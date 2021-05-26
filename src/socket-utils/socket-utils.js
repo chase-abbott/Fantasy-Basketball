4124cb1fd8ff485dc7596fe60ec1281037441939
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-26 09:49:30 -0700

    heroku config attempt

--- src/socket-utils/socket-utils.js
+++ src/socket-utils/socket-utils.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 // const SOCKET_URL = 'http://localhost:3000/';
 const SOCKET_URL = 'https://fathomless-springs-43889.herokuapp.com/';
 const socket = io(SOCKET_URL);
-
+//
 export function socketLogIn(user) {
   socket.emit('logged-in', user);
 }
