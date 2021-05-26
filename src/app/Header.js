1d26843ad9eb8d24a9f909413d4adfcf6fd6f677
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-26 16:35:09 -0700

    logout link

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
-
+  handleLogOut = () => {
+    window.localStorage.clear();
+    window.location.reload();
+  }
   render() { 
     return (
       <header className="Header">
         <Link to='/auth'>Login/Sign Up</Link>
         <Link to='/draft'>Click here to enter your draft</Link>
         <Link to='/myteam'>My Team</Link>
+        <Link to='/auth' onClick={this.handleLogOut}>Log Out</Link>
       </header>
     );
   }
@@ -17,3 +21,4 @@ class Header extends Component {
 }
  
 export default Header;
+//
