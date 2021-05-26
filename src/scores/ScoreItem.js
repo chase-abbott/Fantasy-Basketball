7762d33725478caefa45deeb75ea896cb1068e54
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-25 22:17:25 -0700

    score layout change

--- src/scores/ScoreItem.js
+++ src/scores/ScoreItem.js
@@ -8,11 +8,11 @@ export default class ScoreItem extends Component {
     const { score } = this.props;
     return (
       <li className="ScoreItem">
+        <h2 className='homeTeam'>{score.homeTeam}: <span>{score.homeScore}</span></h2> 
         <h2 className='status'>{score.status}</h2>
         <h2 className='quarter'>QTR: {score.quarter}</h2>
         <h2 className='time'>{score.minutes}:{score.seconds}</h2>
-        <h2 className='homeTeam'>{score.homeTeam}: <span>{score.homeScore}</span></h2> 
-        <h2>{score.awayTeam}: {score.awayScore}</h2>
+        <h2 className='awayTeam'>{score.awayTeam}: <span>{score.awayScore}</span></h2>
         <h2>Projected Spread: {score.spread}</h2>
         <h2>Projected Over/Under: {score.overUnder}</h2>
         
